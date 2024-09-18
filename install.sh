0a5a4dc682f778f29adb7f18f195bc93cb8936ca
nuck nuck <88329848+nuck0nuck@users.noreply.github.com>
2024-09-19 01:10:09 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-mkdir marzback;
-cd marzback;
 sudo apt update && apt upgrade -y;
 sudo apt-get install php -y;
 sudo apt-get install php-curl -y;
+mkdir marzback;
+cd marzback;
 rm menu.php;
 wget https://raw.githubusercontent.com/nuck0nuck/marzback/refs/heads/main/menu.php;
 php menu.php;
