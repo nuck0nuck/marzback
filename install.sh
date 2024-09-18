d9cdb000ea62a8e48bce6910f4ab2b5c38dbfbf7
nuck nuck <88329848+nuck0nuck@users.noreply.github.com>
2024-09-19 00:46:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-sudo apt-get install php8.1-fpm -y;
+sudo apt update && apt upgrade -y;
+sudo apt-get install php -y;
+sudo apt-get install php-curl -y;
 rm menu.php;
 wget https://raw.githubusercontent.com/nuck0nuck/marzback/refs/heads/main/menu.php;
 php menu.php;
